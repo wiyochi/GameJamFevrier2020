033faf4ebc051213bae7fb8ab8a34890ef20c594
Jérémy Zangla <zangla.jeremy@gmail.com>
2020-02-15 11:51:31 +0100

    Update .gitignore & Player move in diag avec clavier

--- src/engine/Player.cpp
+++ src/engine/Player.cpp
@@ -1,5 +1,7 @@
 #include "Player.hpp"
 
+float Player::diag_speed = max_speed * max_speed / sqrt(max_speed * max_speed + max_speed * max_speed);
+
 Player::Player()
 {
 
@@ -7,13 +9,55 @@ Player::Player()
 
 void Player::update()
 {
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z))
-        move(0, -5);
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
-        move(-5, 0);
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-        move(0, 5);
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-        move(5, 0);
-    
+
+    if (sf::Joystick::isConnected(0))
+    {
+        float x = sf::Joystick::getAxisPosition(0, sf::Joystick::X);
+        float y = sf::Joystick::getAxisPosition(0, sf::Joystick::Y);
+       
+        if (abs(x) < zero_zone) x = 0;
+        if (abs(y) < zero_zone) y = 0;
+        move(x * speed_coef , y * speed_coef);
+    } else 
+    {
+        if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Z) && sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+            || (!sf::Keyboard::isKeyPressed(sf::Keyboard::Z) && !sf::Keyboard::isKeyPressed(sf::Keyboard::S)))
+        {
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) // Diag gauche
+            {
+                if (!sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+                    move(-max_speed, 0);
+            }
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) // Diag gauche
+            {
+                if (!sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
+                    move(max_speed, 0);
+            }
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z)) // Monte
+        {
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) // Diag gauche
+            {
+                if (!sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+                    move(-diag_speed, -diag_speed);
+            }
+            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) // Diag droite
+                move(diag_speed, -diag_speed);
+            else
+                move(0, -max_speed);
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) // Descend
+        {
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) // Diag gauche
+            {
+                if (!sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+                    move(-diag_speed, diag_speed);
+            }
+            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) // Diag droite
+                move(diag_speed, diag_speed);
+            else 
+                move(0, max_speed);
+        }
+    }
 }
--- src/engine/Player.hpp
+++ src/engine/Player.hpp
@@ -1,9 +1,17 @@
 #pragma once
 
+#include <iostream>
+#include <cmath>
 #include "Entity.hpp"
+
 
 class Player : public Entity
 {
+private:
+    static constexpr float max_speed = 7.5f;
+    static float diag_speed;
+    static constexpr float speed_coef = max_speed / 100;
+    static constexpr int zero_zone = 10;
 public:
     Player();
     void update() override;
